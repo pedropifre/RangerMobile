2d3817f91bd90de44f83ada974a3c3f5e33ab4ec
pedropifre <pedropifre@gmail.com>
2023-02-19 04:03:56 -0300

    FixedUIDamage
    Change the Damage UI color to red and opacity to 0 in the last 20% of it

--- Assets/Scripts/Enemy/TextDamageBase.cs
+++ Assets/Scripts/Enemy/TextDamageBase.cs
@@ -21,7 +21,7 @@ public class TextDamageBase : MonoBehaviour
     public void spawn()
     {
         setNumber(health);
-        ShowNumber();
+        StartCoroutine(ShowNumber());
         Destroy(gameObject, duration + 0.1f);
     }
 
@@ -30,13 +30,16 @@ public class TextDamageBase : MonoBehaviour
         textToUse.text = life.ToString();
     }
 
-    public void ShowNumber()
+    IEnumerator ShowNumber()
     {
         //animation
         textToUse.transform.localScale = new Vector2(0, 0);
         textToUse.transform.DOScale(new Vector2(1, 1), duration).SetLoops(-1, LoopType.Yoyo);
         textToUse.transform.DOLocalMove(new Vector2(enemy.transform.position.x+
             Random.Range(-44, 44), enemy.transform.position.y+44), duration);
+        textToUse.DOColor(Color.red, 0);
+        yield return new WaitForSeconds(duration*0.8f);
+        textToUse.DOFade(0f,duration*0.2f);
     }
 
     
